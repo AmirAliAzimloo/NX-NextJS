4e75a6fb8c857c4b20de841bbf1af7c3ab8830ad
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-04-27 22:33:08 +0430

    Add shared drop-down using shadcn as monorepo package.

--- apps/nx-next-js/src/app/page.tsx
+++ apps/nx-next-js/src/app/page.tsx
@@ -1,11 +1,36 @@
 import styles from './page.module.css';
-import { Button } from '@nx-next-js/ui-components';
-
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@nx-next-js/ui-components';
 
 export default function Index() {
   return (
     <div className={styles.page}>
-      <Button >Hi! I'm shared button</Button>
+      <div
+      className='flex flex-col gap-5'
+      >
+
+      
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+        <Button>Hi! I'm shared button</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      </div>
     </div>
   );
 }
