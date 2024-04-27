795c652e4a8a0a201518c6317d6d183560d4baa4
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-04-27 22:16:16 +0430

    Add Shadcn-UI as reusable module.

--- apps/nx-next-js/src/app/page.tsx
+++ apps/nx-next-js/src/app/page.tsx
@@ -1,10 +1,11 @@
 import styles from './page.module.css';
+import { Button } from '@nx-next-js/ui-components';
 
 
 export default function Index() {
   return (
     <div className={styles.page}>
-      home page
+      <Button >Hi! I'm shared button</Button>
     </div>
   );
 }
